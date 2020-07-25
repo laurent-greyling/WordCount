9b2611bd209acc5080961c7e03abc0c50a3ba0c3
Laurent Greyling <laurent.greyling@smartlockr.eu>
2020-07-25 19:56:51 +0200

    More logging and error handling

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -13,30 +13,52 @@ namespace WordCount
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(o =>
                 {
-                    if (!string.IsNullOrWhiteSpace(o.TextToUse) && !string.IsNullOrWhiteSpace(o.WordToUse))
+                    if (string.IsNullOrWhiteSpace(o.TextToUse) && string.IsNullOrWhiteSpace(o.WordToUse) && string.IsNullOrWhiteSpace(o.WordToUse))
                     {
-                        var wordFrequency = wordFrequencyAnalyzer.CalculateFrequencyForWord(o.TextToUse, o.WordToUse);
-                        Console.WriteLine($"The word '{o.WordToUse}' occurred '{wordFrequency}' times");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("No valid arguments for evaluation provided ");
+                        return; 
                     }
 
-                    if (!string.IsNullOrWhiteSpace(o.TextToUse))
-                    {
-                        var highestFrequency = wordFrequencyAnalyzer.CalculateHighestFrequency(o.TextToUse);
-                        Console.WriteLine($"The highest frequency of a word in this paragraph is '{highestFrequency}' times");
-                    }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Starting process");
+                    Console.ForegroundColor = ConsoleColor.White;
 
-                    if (!string.IsNullOrWhiteSpace(o.TextToUse) && o.WordOnAverageCount > 0)
+                    try
                     {
-                        var wordsOnNthOccurence = wordFrequencyAnalyzer.CalculateMostFrequentNWords(o.TextToUse, o.WordOnAverageCount);
-                        Console.WriteLine($"Words that occured n={o.WordOnAverageCount} times:");
-
-                        foreach (var item in wordsOnNthOccurence)
+                        if (!string.IsNullOrWhiteSpace(o.TextToUse) && !string.IsNullOrWhiteSpace(o.WordToUse))
                         {
-                            Console.WriteLine($"{item.Word} : {item.Frequency}");
+                            var wordFrequency = wordFrequencyAnalyzer.CalculateFrequencyForWord(o.TextToUse, o.WordToUse);
+                            Console.WriteLine($"The word '{o.WordToUse}' occurred '{wordFrequency}' times");
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(o.TextToUse))
+                        {
+                            var highestFrequency = wordFrequencyAnalyzer.CalculateHighestFrequency(o.TextToUse);
+                            Console.WriteLine($"The highest frequency of a word in this paragraph is '{highestFrequency}' times");
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(o.TextToUse) && o.WordOnAverageCount > 0)
+                        {
+                            var wordsOnNthOccurence = wordFrequencyAnalyzer.CalculateMostFrequentNWords(o.TextToUse, o.WordOnAverageCount);
+                            Console.WriteLine($"Words that occured n={o.WordOnAverageCount} times:");
+
+                            foreach (var item in wordsOnNthOccurence)
+                            {
+                                Console.WriteLine($"{item.Word} : {item.Frequency}");
+                            }
                         }
                     }
+                    catch (Exception e)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"ERROR: {e}");
+                        return;
+                    }                   
                 });
 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Process successfully completed");
             Console.ReadKey();
         }
     }
