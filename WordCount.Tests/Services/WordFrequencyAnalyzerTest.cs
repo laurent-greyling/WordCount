eb23f299f342c9a61c0b913d662b571ca50c7c5d
Laurent Greyling <laurent.greyling@smartlockr.eu>
2020-07-25 12:08:27 +0200

    Frequency analyser tests

---
+++ WordCount.Tests/Services/WordFrequencyAnalyzerTest.cs
@@ -0,0 +1,22 @@
+﻿using WordCount.Services;
+using Xunit;
+
+namespace WordCount.Tests.Services
+{
+    public class WordFrequencyAnalyzerTest
+    {
+        private string _phrase = "Hallo World, the wOrld is ours";
+        private string _word = "world";
+        private WordFrequencyAnalyzer Result;
+
+        [Fact]
+        public void Frequency_Of_Word_Return_Occurence_Of_Specified_Word() 
+        {
+            Result = new WordFrequencyAnalyzer();
+
+            var wordFrequency = Result.CalculateFrequencyForWord(_phrase, _word);
+
+            Assert.Equal(2, wordFrequency);
+        }
+    }
+}
