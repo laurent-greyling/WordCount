6a3cda77dadfa58a6d8f5a1483e99319d739437c
Laurent Greyling <laurent.greyling@smartlockr.eu>
2020-07-25 20:47:49 +0200

    Add large text file and code to read file to evaluate and handle some edgecases

--- WordCount/Options.cs
+++ WordCount/Options.cs
@@ -4,6 +4,9 @@ namespace WordCount
 {
     public class Options
     {
+        /// <summary>
+        /// This has a character limit, so good for small amounts of text
+        /// </summary>
         [Option('t', "text", Required = false, HelpText = "The text that need to be analysed for word frequency")]
         public string TextToUse { get; set; }
 
@@ -12,5 +15,8 @@ namespace WordCount
 
         [Option('n', "nth", Required = false, Default = 0, HelpText = "The most frequent 'n' words in the text")]
         public int WordOnAverageCount { get; set; }
+
+        [Option('p', "path", Required = false, HelpText = "Path to file with text to be analysed")]
+        public string PathToTextFile { get; set; }
     }
 }
--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -1,5 +1,7 @@
 ﻿using CommandLine;
 using System;
+using System.IO;
+using System.Text;
 using WordCount.Factories;
 
 namespace WordCount
@@ -13,34 +15,39 @@ namespace WordCount
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(o =>
                 {
-                    if (string.IsNullOrWhiteSpace(o.TextToUse) && string.IsNullOrWhiteSpace(o.WordToUse) && string.IsNullOrWhiteSpace(o.WordToUse))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("No valid arguments for evaluation provided ");
-                        return; 
-                    }
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Starting process");
-                    Console.ForegroundColor = ConsoleColor.White;
-
                     try
                     {
-                        if (!string.IsNullOrWhiteSpace(o.TextToUse) && !string.IsNullOrWhiteSpace(o.WordToUse))
+                        if (string.IsNullOrWhiteSpace(o.TextToUse) && string.IsNullOrWhiteSpace(o.WordToUse) && string.IsNullOrWhiteSpace(o.WordToUse) && string.IsNullOrWhiteSpace(o.PathToTextFile))
                         {
-                            var wordFrequency = wordFrequencyAnalyzer.CalculateFrequencyForWord(o.TextToUse, o.WordToUse);
+                            throw new Exception("No valid arguments for evaluation provided");
+                        }
+
+                        var textToEvaluate = TextToEvalate(o.TextToUse, o.PathToTextFile);
+
+                        if (string.IsNullOrWhiteSpace(textToEvaluate))
+                        {
+                            throw new Exception("No text provided to evaluate");
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Starting process");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        if (!string.IsNullOrWhiteSpace(textToEvaluate) && !string.IsNullOrWhiteSpace(o.WordToUse))
+                        {
+                            var wordFrequency = wordFrequencyAnalyzer.CalculateFrequencyForWord(textToEvaluate, o.WordToUse);
                             Console.WriteLine($"The word '{o.WordToUse}' occurred '{wordFrequency}' times");
                         }
 
-                        if (!string.IsNullOrWhiteSpace(o.TextToUse))
+                        if (!string.IsNullOrWhiteSpace(textToEvaluate))
                         {
-                            var highestFrequency = wordFrequencyAnalyzer.CalculateHighestFrequency(o.TextToUse);
+                            var highestFrequency = wordFrequencyAnalyzer.CalculateHighestFrequency(textToEvaluate);
                             Console.WriteLine($"The highest frequency of a word in this paragraph is '{highestFrequency}' times");
                         }
 
-                        if (!string.IsNullOrWhiteSpace(o.TextToUse) && o.WordOnAverageCount > 0)
+                        if (!string.IsNullOrWhiteSpace(textToEvaluate) && o.WordOnAverageCount > 0)
                         {
-                            var wordsOnNthOccurence = wordFrequencyAnalyzer.CalculateMostFrequentNWords(o.TextToUse, o.WordOnAverageCount);
+                            var wordsOnNthOccurence = wordFrequencyAnalyzer.CalculateMostFrequentNWords(textToEvaluate, o.WordOnAverageCount);
                             Console.WriteLine($"Words that occured n={o.WordOnAverageCount} times:");
 
                             foreach (var item in wordsOnNthOccurence)
@@ -54,12 +61,43 @@ namespace WordCount
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine($"ERROR: {e}");
                         return;
-                    }                   
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Process successfully completed");                   
                 });
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Process successfully completed");
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// If we provide text and a file of large text, we append them and evaluate the entire string
+        /// Will also return one or the other if only one was provided
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="pathToTextFile"></param>
+        /// <returns></returns>
+        private static string TextToEvalate(string text, string pathToTextFile)
+        {
+            if (string.IsNullOrWhiteSpace(text) && string.IsNullOrWhiteSpace(pathToTextFile))
+            {
+                return string.Empty;
+            }
+
+            var textToEvaluate = new StringBuilder();
+            textToEvaluate.Append(text);
+
+            //If there is a text file and we do not append a space to the initial string, the words will
+            //be against each other and not counted. This is to allow so they are seen as independent
+            textToEvaluate.Append(" ");
+
+            if (!string.IsNullOrWhiteSpace(pathToTextFile))
+            {
+                var textInFile = File.ReadAllText(pathToTextFile);
+                textToEvaluate.Append(textInFile);
+            }
+            
+            return textToEvaluate.ToString();
         }
     }
 }
